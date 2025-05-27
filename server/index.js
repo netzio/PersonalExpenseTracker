89ee4f625f92b4fc6d5e5a039cbcc1e30a0a3516
Neo Masemola <masemolaneo4@gmail.com>
2025-05-27 23:17:05 +0200

    Setup prisma as an ORM

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+prisma.$connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Database connection error:', error);
+  });
+
 
 const app = express();
 app.use(cors());
